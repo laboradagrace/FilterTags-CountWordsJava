9b2d8ae801fc2b8b984ea3a2505a4f77708761c6
laboradagr_sd2082@CEACCS.NET <laboradagr_sd2082@CEACCS.NET>
2019-11-06 16:42:21 +0800

    commited

---
+++ MyFisrtApp/src/myfisrtapp/MyFisrtApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myfisrtapp;
+
+/**
+ *
+ * @author laboradagr_sd2082
+ */
+public class MyFisrtApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
